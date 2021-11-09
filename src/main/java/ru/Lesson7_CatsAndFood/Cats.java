f7ad991f3ec8166197327017f8c991ae6fa22681
ZarVee <zarvee7@gmail.com>
2021-11-09 20:19:46 +0500

    Lesson7

--- src/main/java/ru/Lesson7_CatsAndFood/Cats.java
+++ src/main/java/ru/Lesson7_CatsAndFood/Cats.java
@@ -2,9 +2,9 @@ package ru.Lesson7_CatsAndFood;
 
 class Cats {
 
-    String name;
-    Boolean satiety;
-    int appetite;
+    private String name;
+    private Boolean satiety;
+    private int appetite;
 
     protected Cats(String name, Boolean satiety, int appetite){
         this.appetite = appetite;
@@ -18,5 +18,9 @@ class Cats {
 
     public int getAppetite() {
         return appetite;
+    }
+
+    public void setSatiety(Boolean satiety) {
+        this.satiety = satiety;
     }
 }
